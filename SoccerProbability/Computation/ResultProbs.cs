54fb61f365c9745c0db955668c06377398c3ff22
Alexander Bocharov <alexbocharov2@gmail.com>
2020-07-06 00:03:17 +0300

    Added classes

---
+++ SoccerProbability/Computation/ResultProbs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoccerProbability.Computation
+{
+    /// <summary>
+    /// Содержит вероятности исходов
+    /// </summary>
+    class ResultProbs
+    {
+
+    }
+}
