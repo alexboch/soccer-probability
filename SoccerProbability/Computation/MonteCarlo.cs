802c503e57037ad9e9c6c6f24be7bcb822a2c028
Alexander Bocharov <alexbocharov2@gmail.com>
2020-07-16 01:59:38 +0300

    Monte Carlo method

---
+++ SoccerProbability/Computation/MonteCarlo.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoccerProbability.Computation
+{
+    public class MonteCarlo
+    {
+        public ResultProbs Generate(InputData inputData, int numMatches)
+        {
+            var meanIntensityHost = inputData.MeanIntensityHost;
+            var meanIntensityGuest = inputData.MeanIntensityGuest;
+            var rand = new Random();
+            int numHostsWin = 0;
+            int numGuestsWin = 0;
+            int numNotFinished = 0;
+            int numDraws = 0;
+            int goalsRemain = inputData.GoalsRemain;
+            for (int i = 0; i < numMatches; i++)
+            {
+                var numGoalsHost = inputData.Goals.Count(g => g is GoalType.Host);
+                var numGoalsGuest = inputData.Goals.Count(g => g is GoalType.Guest);
+                if (goalsRemain > 0)
+                {
+                    for (int j = 0; j < inputData.MinutesTillEnd; j++)
+                    {
+                        var r1 = rand.NextDouble();
+                        var r2 = rand.NextDouble();
+
+                        if (r1 <= meanIntensityHost)
+                        {
+                            //Гол хозяев
+                            numGoalsHost++;
+                            if (numGoalsHost + numGoalsGuest == goalsRemain)
+                            {
+                                break;
+                            }
+                        }
+
+                        if (r2 <= meanIntensityGuest)
+                        {
+                            //Гол гостей
+                            numGoalsGuest++;
+                            if (numGoalsHost + numGoalsGuest == goalsRemain)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                if (numGoalsHost + numGoalsGuest < goalsRemain)
+                {
+                    numNotFinished++;
+                }
+
+                if (numGoalsHost > numGoalsGuest)
+                {
+                    numHostsWin++;
+                }
+
+                if (numGoalsGuest < numGoalsHost)
+                {
+                    numGuestsWin++;
+                }
+
+                if (numGoalsGuest == numGoalsHost)
+                {
+                    numDraws++;
+                }
+
+            }
+
+            var hostsWonFrac = (double)numHostsWin / numMatches;
+            var guestsWonFrac = (double)numGuestsWin / numMatches;
+            var drawFrac = (double)numDraws / numMatches;
+            var notFinishedFrac = (double) numNotFinished / numMatches;
+            var res = new ResultProbs(hostsWonFrac, guestsWonFrac, drawFrac, notFinishedFrac);
+            return res;
+        }
+
+
+    }
+}
